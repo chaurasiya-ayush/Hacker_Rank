6f091c8be708bf558ff8190b69872b3dfe3524c4
Ayush-Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-03-12 00:08:41 +0530

    Bitwise_Multiply By 3.5

--- Bitwise_Multiply By 3.5.c
+++ Bitwise_Multiply By 3.5.c
@@ -5,7 +5,8 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     int n;
     scanf("%d",&n);
-    n= ((n|(n+1)));
-    printf("%d",n);
+    n = ((n+ (n<<1))+(n>>1));
+    printf("%d ",n);
+    
     return 0;
 }
