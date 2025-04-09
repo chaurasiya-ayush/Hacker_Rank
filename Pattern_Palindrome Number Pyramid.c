677f43522c3481078467c06dab6920f586f2d93f
Ayush-Chaurasiya <chaurasiyaayush2006@gmail.com>
2025-04-09 07:42:54 +0530

    Pattern_Palindrome Number Pyramid

---
+++ Pattern_Palindrome Number Pyramid.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n-i;k++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            if(j<=i)
+                printf("%d",j);
+            else  {
+                printf("%d",2*i-j);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
